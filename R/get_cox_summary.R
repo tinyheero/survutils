f1c55b03d15ccf8684d67ba04dd3c7fe5db23a60
fongchun <fongchunchan@gmail.com>
2015-07-22 17:17:30 -0700

    Adding Confidence Interval Parameter

--- R/get_cox_summary.R
+++ R/get_cox_summary.R
@@ -3,18 +3,22 @@
 #' This function summarizes the results of a cox regression returning the
 #' results in a data.frame. It will calculate the confidence interval.
 #' 
-#' @param cox.res A cox regression analysis results object.
+#' @param cox.res The result of a coxph fit.
+#' @param ci The confidence interval to calculate.
 #' @return A data.frame that summarizes the results.
 #' @export
 #' @examples
 #' cox.res <- survival::coxph(survival::Surv(time,status) ~ rx, survival::colon)
 #' get_cox_summary(cox.res)
 # Description: This function is used to get the cox-regression results
-get_cox_summary <- function(cox.res){
+get_cox_summary <- function(cox.res, ci = 95){
 	beta <- coef(cox.res)
 	se <- sqrt(diag(vcov(cox.res)))
 	HR <- exp(beta)
 	HRse <- HR * se
+
+  # Calculate the tail to use for the confidence interval
+  ci.tail <- 1 - ((100 - ci)/2)/100
 
 	outDf <- data.frame(coef = beta, 
                       se = se, 
@@ -24,8 +28,8 @@ get_cox_summary <- function(cox.res){
                       HRse = HRse,
                       HRz = (HR - 1) / HRse, 
                       HRp = 1 - pchisq(((HR - 1)/HRse)^2, 1),
-                      HRCILL = exp(beta - qnorm(.975, 0, 1) * se),
-                      HRCIUL = exp(beta + qnorm(.975, 0, 1) * se))
+                      HRCILL = exp(beta - qnorm(ci.tail, 0, 1) * se),
+                      HRCIUL = exp(beta + qnorm(ci.tail, 0, 1) * se))
   outDf <- round(outDf, 3)
   outDf
 } 
