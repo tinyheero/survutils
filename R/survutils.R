1f4cdb916a6e2f960328fead14cee540fcae922b
Fong Chun Chan <fongchun@alumni.ubc.ca>
2017-03-20 14:09:56 -0700

    Fixing devtools::check()

--- R/survutils.R
+++ R/survutils.R
@@ -2,5 +2,13 @@
 #' 
 #' @docType package
 #' @importFrom magrittr %>%
+#' @importFrom stats as.formula
+#' @importFrom stats coef
+#' @importFrom stats pchisq
+#' @importFrom stats qnorm
+#' @importFrom stats setNames
+#' @importFrom stats stepfun
+#' @importFrom stats time
+#' @importFrom stats vcov
 #' @name survutils
 NULL
